b4304a01eafc55fcf58a5e1863b752c35b0e7c29
coder-isiahjones <jones.isiah.blhs@gmail.com>
2023-03-19 18:13:35 -0400

    update with eslint

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Rection Time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Fast Reaction Time";
+    } else if (this.score < 400) {
+      this.rank = "Decent Reaction Time";
+    } else {
+      this.rank = "Slow Reaction Time";
+    }
+  },
+};
+</script>
+
+<style scoped>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
